97deddf3a91b87b1e003f32d0915c6872f23ad2f
Mohil2020 <mohilmoliya2020@gmail.com>
2024-02-02 22:55:39 +0530

    api calling-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Posts from './Pages/Post/Posts';
+import Header from './Component/header/Header';
+import Comments from './Pages/Comments/Comments';
+import Photos from './Pages/Photos/Photos';
+import { Routes, Route } from'react-router-dom';
+import Album from './Pages/Album/Album';
+import Todo from './Pages/Todos/Todo';
+import Users from './Pages/Users/Users';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <div className="App">
+      <Routes>
+        <Route path="/" element={ <Posts/> } />
+        <Route path="/Comments" element={ <Comments/> } />
+        <Route path="/Photos" element={ <Photos/> } />
+        <Route path="/Album" element={ <Album/> } />
+        <Route path="/todos" element={ <Todo/> } />
+        <Route path="/user" element={ <Users/> } />
+      </Routes>
+
+    </div>
     </div>
   );
 }
